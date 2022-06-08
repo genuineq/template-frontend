a1e21730721e3519fb135825f9b1f3d93988cee8
StefanGhiban <stefanghiban962@gmail.com>
2022-06-08 16:30:15 +0300

    suing formkit button for consistency

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -41,15 +41,11 @@ const now = useNow();
         {{ now.toLocaleTimeString() }}
     </div>
     {{ $t("test") }}
-    <div>
-        <button
-            type="button"
-            @click="testModal = true"
-            class="rounded-md bg-black bg-opacity-20 px-4 py-2 text-sm font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
-        >
-            Open dialog
-        </button>
-    </div>
+    <FormKit
+        type="button"
+        @click="testModal = true"
+        label="OpenDialog"
+    />
     <BaseModal :opened="testModal" @close="testModal = false">
         <template v-slot:title> TITLE </template>
     </BaseModal>
