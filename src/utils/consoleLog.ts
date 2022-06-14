3d52fd93a6121774ac94f36d3d2e67aa9e97a6ed
StefanGhiban <stefanghiban962@gmail.com>
2022-06-14 09:41:00 +0300

    added comment to explain and also solve eslint error

--- src/utils/consoleLog.ts
+++ src/utils/consoleLog.ts
@@ -1,1 +1,8 @@
-if (import.meta.env.MODE === "production") console.log = function () {};
+if (import.meta.env.MODE === "production")
+    console.log = function () {
+        /**
+         * We override the console.log function in production
+         * so we dont have any accidental data written to the
+         * console.
+         */
+    };
