d99363d2f0c5f57d307fa2746ac5b857ab650f73
StefanGhiban <stefanghiban962@gmail.com>
2022-06-14 09:41:16 +0300

    added some variables from the useAxios wrapper to the http service

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -3,10 +3,10 @@ import { useAxios } from "@vueuse/integrations/useAxios";
 type Method = "GET" | "POST" | "PUT" | "DELETE";
 
 export const http = async (method: Method, route: string, dataObject = {}) => {
-    const { data, error } = await useAxios(route, {
+    const { data, error, isLoading, isFinished } = await useAxios(route, {
         method: method,
         data: dataObject,
     });
 
-    return { data, error };
+    return { data, error, isLoading, isFinished };
 };
