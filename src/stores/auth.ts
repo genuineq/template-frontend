e6a083ef5196d3b4851ff1d653cc87a93ec1cf1c
StefanGhiban <stefanghiban962@gmail.com>
2022-06-14 10:13:44 +0300

    typed this with remoavble ref to stop error

--- src/stores/auth.ts
+++ src/stores/auth.ts
@@ -1,11 +1,11 @@
 import { defineStore } from "pinia";
-import { useLocalStorage } from "@vueuse/core";
+import { useLocalStorage, type RemovableRef } from "@vueuse/core";
 
 /**
  * This is an composition api type of store, similar to setup function in vue 3.
  */
 export const useAuthStore = defineStore("auth", () => {
-    const token = useLocalStorage("authUser", "");
+    const token: RemovableRef<string> = useLocalStorage("authUser", "");
 
     return { token };
 });
