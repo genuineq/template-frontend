c619d9223da7a776fca85523368583753c47f884
StefanGhiban <stefanghiban962@gmail.com>
2022-06-14 10:30:11 +0300

    comments

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -2,6 +2,14 @@ import { useAxios } from "@vueuse/integrations/useAxios";
 
 type Method = "GET" | "POST" | "PUT" | "DELETE";
 
+/**
+ * Http wrapper for the network service in case we want to change
+ * the underlying technology (in this case axios)
+ * @param { Method }  method the method for the call Post, Put, Get, Delete
+ * @param {string} route the route for the call, should be only the resource as the base url is already configured
+ * @param {object} dataObject object for sending data to the server.
+ * @returns
+ */
 export const http = async (method: Method, route: string, dataObject = {}) => {
     const { data, error, isLoading, isFinished } = await useAxios(route, {
         method: method,
