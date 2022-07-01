dc3cb7683a1f5421d40ff036430c69f14b902590
StefanGhiban <stefanghiban962@gmail.com>
2022-07-01 14:50:54 +0300

    added example config if we want to use env vars that are not exposed

--- vite.config.ts
+++ vite.config.ts
@@ -15,3 +15,29 @@ export default defineConfig({
         port: 3000,
     },
 });
+
+/**
+ * Config if we want to use the env vars that are not exposed to Vite
+ */
+/*
+import { fileURLToPath, URL } from "url";
+
+import { defineConfig, loadEnv } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+// https://vitejs.dev/config/
+export default defineConfig(({ command, mode }) => {
+    const env = loadEnv(mode, process.cwd(), "");
+    return {
+        plugins: [vue()],
+        resolve: {
+            alias: {
+                "@": fileURLToPath(new URL("./src", import.meta.url)),
+            },
+        },
+        server: {
+            port: Number(env.PORT),
+        },
+    };
+});
+*/
